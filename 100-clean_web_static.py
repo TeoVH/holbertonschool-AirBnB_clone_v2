79b93259aae8402d4dbede815da86db5ffbc6bd7
TeoVH <villadamateo64@gmail.com>
2022-09-11 23:42:34 -0500

    Script that deletes out-of-date archives

---
+++ 100-clean_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that deletes out-of-date archives
+"""
+
+from fabric.api import *
+import os
+from datetime import datetime
+import tarfile
+
+
+env.hosts = ['3.208.25.96', '3.89.66.142']
+
+
+def do_clean(number=0):
+    """ Removes all but given number of archives"""
+    number = int(number)
+    if number < 2:
+        number = 1
+    number += 1
+    number = str(number)
+    with lcd("versions"):
+        local("ls -1t | grep web_static_.*\.tgz | tail -n +" +
+              number + " | xargs -I {} rm -- {}")
+    with cd("/data/web_static/releases"):
+        run("ls -1t | grep web_static_ | tail -n +" +
+            number + " | xargs -I {} rm -rf -- {}")
